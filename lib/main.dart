70b71ecad5b2ef474db5a7c408db0c92235fb0a8
philipjan <philipjanbaruis@gmail.com>
2025-08-05 20:40:12 +0800

    learn_riverpod - counter using riverpod working without no annotations

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,67 @@
+import 'package:cubitbloc/simple_state_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => App();
+void main() {
+  runApp(
+    // add riverpod ProviderScope to the root widget of  the app; A Must
+      const ProviderScope(child: App(),)
+  );
+}
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+  
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple Riverpod!",),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            children: [
+              Text(
+                ref.watch(riverPodCountState).toString(),
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              ElevatedButton.icon(
+                  onPressed: () {
+                    ref.read(riverPodCountState.notifier).state++;
+                  },
+                icon: const Icon(Icons.add),
+                label: const Text("Add"),
+              ),
+
+              SizedBox(height: 8,),
+
+              ElevatedButton.icon(
+                onPressed: () {
+                  ref.read(riverPodCountState.notifier).state--;
+                },
+                icon: const Icon(Icons.remove),
+                label: const Text("Remove"),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/simple_state_provider.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter_riverpod/legacy.dart';
+
+final riverPodCountState = StateProvider<int>((ref) {
+  return 0;
+});
+
