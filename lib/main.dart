684af1bed456db2a36502ccd048420ad9d8179fa
philipjan <philipjanbaruis@gmail.com>
2025-08-11 20:41:57 +0800

    learn_riverpod_todo - added freezed_annotation - added json_annotation - added freezed - added json_serializable - added initial layout with todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:cubitbloc/simple_state_provider.dart';
+import 'package:cubitbloc/widget/widget_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     // add riverpod ProviderScope to the root widget of  the app; A Must
-      const ProviderScope(child: App(),)
+    const ProviderScope(child: App()),
   );
 }
 
@@ -14,52 +14,21 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
+    return const MaterialApp(home: HomePage());
   }
 }
 
-
-class HomePage extends ConsumerWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Simple Riverpod!",),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 20),
-          child: Column(
-            children: [
-              Text(
-                // ref.watch(riverPodCountState).toString(),
-                ref.watch(counterProvider).toString(),
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              ElevatedButton.icon(
-                  onPressed: () {
-                    ref.read(counterProvider.notifier).increment();
-                  },
-                icon: const Icon(Icons.add),
-                label: const Text("Add"),
-              ),
-
-              SizedBox(height: 8,),
-
-              ElevatedButton.icon(
-                onPressed: () {
-                  ref.read(counterProvider.notifier).decrement();
-                },
-                icon: const Icon(Icons.remove),
-                label: const Text("Remove"),
-              ),
-              
-            ],
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("TODO async Riverpod"), centerTitle: true),
+        body: Center(
+          child: ListView.builder(
+            itemBuilder: (ctx, index) => TodoItem(key: Key(index.toString())),
           ),
         ),
       ),
---
+++ lib/widget/widget_list_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// TODO: 8/11/25 Finalize the content
+class TodoItem extends StatelessWidget {
+  const TodoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
